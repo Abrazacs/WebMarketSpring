459f53d34e1ee932575f01c34976179d2046f332
Abrazacs <sergeysemenov87@gmail.com>
2022-04-11 00:18:14 +0300

    Initial commit

---
+++ src/main/java/ru/sergeysemenov/webmarketspring/WebMarketSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.sergeysemenov.webmarketspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMarketSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebMarketSpringApplication.class, args);
+    }
+
+}
---
+++ src/main/resources/static/index.js
@@ -0,0 +1,40 @@
+angular.module('market', []).controller('indexController', function ($scope, $http) {
+    $scope.fillTable = function () {
+        $http.get('http://localhost:8080/market/api/v1/products')
+            .then(function (response) {
+                $scope.products = response.data;
+            });
+    }
+
+    $scope.fillCart = function (){
+        $http.get('http://localhost:8080/market/api/v1/cart/show-cart')
+            .then(function (response){
+                $scope.productsInCart = response.data;
+            });
+    }
+
+    $scope.deleteProduct = function (id) {
+        $http.delete('http://localhost:8080/market/api/v1/products/' + id)
+            .then(function (response) {
+                $scope.fillTable();
+            });
+    }
+
+    $scope.createNewProduct = function () {
+        $http.post('http://localhost:8080/market/api/v1/products', $scope.newProduct)
+            .then(function (response) {
+                $scope.newProduct = null;
+                $scope.fillTable();
+            });
+    }
+
+    $scope.addProduct = function (id){
+        $http.get('http://localhost:8080/market/api/v1/cart/add-to-cart/'+id)
+            .then(function (response){
+                $scope.fillCart();
+            });
+    }
+
+    $scope.fillTable();
+    $scope.fillCart();
+})
